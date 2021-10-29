bc81a474bd9e58b03569e88c00f065dbf6ea545c
herojia <hodalaplus@gmail.com>
2021-10-30 04:01:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const routes = require("./routes")
+require("./config/mongoose")
+
 const app = express()
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
