90dcdde97eb1134e09749f23d062dcd16482e849
herojia <hodalaplus@gmail.com>
2021-10-30 03:35:19 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const routes = require("./routes")
 const app = express()
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
+app.set("view engine", "hbs")
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(routes)
 
