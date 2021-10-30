c9c7192b5e9a89ca945fcaf2579e2aac6a6b5cf2
herojia <hodalaplus@gmail.com>
2021-10-30 22:50:46 +0800

    add static file

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ require("./config/mongoose")
 
 const app = express()
 
+// setup handlebars
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
 app.set("view engine", "hbs")
+
+// setup static-file path
+app.use(express.static("public"))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(routes)
