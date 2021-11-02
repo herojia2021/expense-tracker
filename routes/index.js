91c187c411305e82bf755891093a44af82d27fd3
herojia <hodalaplus@gmail.com>
2021-11-03 03:16:09 +0800

    feat: handle route not found

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const expenses = require("./modules/expenses")
 const home = require("./modules/home")
 const users = require("./modules/users")
+const error = require("./modules/error")
 const { authenticator } = require("../middleware/auth")
 
 router.use("/expenses", authenticator, expenses)
 router.use("/users", users)
 router.use("/", authenticator, home)
+router.use("*", error)
 
 module.exports = router
